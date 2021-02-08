b54f941408dec28e283fd9ddfadd9d463e510443
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2021-02-08 23:27:34 +0100

    Release srg4+b2 with debug symbols included

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 import PackageDescription
 
 struct PackageMetadata {
-    static let version: String = "4.4.1-srg4+b1"
-    static let checksum: String = "0c0fd2abdc2b2d0e6a2ff075c1850d17f09f8eee274cfcf0f20aea454ff17b70"
+    static let version: String = "4.4.1-srg4+b2"
+    static let checksum: String = "9c503cbf59502b1e4c2c8ebbfb4d24fa1ff92d9bdc147fe3dae98ee824c7246a"
 }
 
 let package = Package(
